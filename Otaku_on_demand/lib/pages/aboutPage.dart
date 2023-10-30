6ac2fd7d545f9f936e7f67fda8be6185e55b4b5b
GabrielLarena <eu7eu20@gmail.com>
2023-10-30 20:11:15 -0300

    update do about page

--- Otaku_on_demand/lib/pages/aboutPage.dart
+++ Otaku_on_demand/lib/pages/aboutPage.dart
@@ -50,8 +50,7 @@ class _AboutPageState extends State<AboutPage> {
                         fontWeight: FontWeight.bold,
                       )),
                   Divider(height: 30),
-                  Text(
-                      "O agravamento da saúde e qualidade de vida e a diminuição do tempo disponível para cuidar de si, são fatores que têm aumentado consideravelmente na rotina da atual sociedade. Ao avaliar a situação, percebe-se que os hábitos alimentares inadequados influenciam e são influenciados fortemente nestas condições. Devido a isso, com o passar dos anos, os humanos cada vez mais incorporam a tecnologia em seu dia a dia,- utilizando o smartphone e suas aplicações como meio principal para obtenção de informações, por meio de sistemas de clusterização. Pensando nisso, este projeto utilizará uma base de dados pública, obtida pelo site Kaggle, que agrega mais de 500 mil receitas por meio das quais serão realizadas análises e a criação de um modelo de clusterização, visando agrupar dados em determinados conjuntos distintos entre si. Por meio disso, a solução prevê, por fim, um aplicativo que categoriza as receitas de acordo com a com opção e desejo de cada usuário para que o mesmo possa manter uma rotina saudável, com refeições práticas e qualidade de vida.")
+                  Text("texto sobre o aplicativo")
                 ])),
           ),
           padding: EdgeInsets.all(50),
