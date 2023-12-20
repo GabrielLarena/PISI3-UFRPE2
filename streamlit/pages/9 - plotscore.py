bba31f724630b9f8990a2fd8c3d613364bb9b61d
GabrielLarena <eu7eu20@gmail.com>
2023-12-20 00:09:11 -0300

    plotscope fix
    streamlit

--- streamlit/pages/9 - plotscore.py
+++ streamlit/pages/9 - plotscore.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 
 # Load Parquet file
-file_path = 'C:\\Users\\eu7eu\\Desktop\\Projetos\\parquet'
-df = pd.read_parquet(file_path)
+df = pd.read_parquet("data/preprocessamento/AnimeList.parquet")
 
 # Filter out rows with score equal to 0
 new_df = df[df['score'] != 0]
@@ -17,10 +16,10 @@ column_name = 'score'
 st.title("Analise de notas")
 
 # KDE plot using Plotly Express
-fig = px.histogram(new_df, x=column_name, marginal="kde", nbins=30, title=f"KDE Plot dos {column_name}s")
+fig = px.histogram(new_df, x=column_name, marginal="kde", nbins=30, title=f"KDE Plot dos {column_name}")
 st.plotly_chart(fig)
 
 # Additional information or analysis (optional)
-st.write("Additional Information:")
+st.write("Informação adicional:")
 st.write(f"Mean {column_name}: {new_df[column_name].mean()}")
 st.write(f"Median {column_name}: {new_df[column_name].median()}")
