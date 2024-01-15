8745503581b6bd1f7c1d7d74a7b2c2f94d85d654
GabrielLarena <eu7eu20@gmail.com>
2024-01-15 13:15:44 -0300

    Revert "Revert "CRUD update - Read, Delete""
    This reverts commit 42e57d4663cb05dc672c560db7f2a6a12021d938.

--- Otaku_on_demand/lib/pages/homePage.dart
+++ Otaku_on_demand/lib/pages/homePage.dart
@@ -1,102 +1,191 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:otaku_on_demand/pages/AnimePage.dart';
+import 'package:otaku_on_demand/pages/listProvider.dart';
 import 'package:otaku_on_demand/model/animemodel.dart';
+import 'package:otaku_on_demand/services/firestore.dart';
 import 'package:otaku_on_demand/services/firestore.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return ChangeNotifierProvider(
+        create: (context) => ListProvider(),
+      child: Scaffold(
         backgroundColor: Color(0xfff2f2f2),
         body: Padding(
         padding: const EdgeInsets.all(8.0),
            child: SingleChildScrollView(
              scrollDirection: Axis.vertical,
              child: HomePageList(),
-    ),));
+            ),
+           ),
+          ),
+        );
+     }
   }
+
+class HomePageList extends StatefulWidget {
+  @override
+  _HomePageListState createState() => _HomePageListState();
 }
 
+class _HomePageListState extends State<HomePageList> {
+  final ScrollController _scrollController = ScrollController();
+  final int initialBatchSize = 16;
+  bool isLoading = false;
 
-class HomePageList extends  StatelessWidget {
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+    _loadInitialData();
+  }
+
+  void _loadInitialData() async {
+    setState(() {
+      isLoading = true;
+    });
+    List<Map<String, dynamic>> initialData =
+    await FirestoreService().getDocumentStream(initialBatchSize, null).first;
+    context.read<ListProvider>().addLoadedItems(initialData);
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  void _onScroll() async {
+    double threshold = 400.0; // Adjust the threshold as needed
+    if (_scrollController.position.pixels >=
+        _scrollController.position.maxScrollExtent / 2 &&
+        !isLoading) {
+      // Load more items when close to the end
+      setState(() {
+        isLoading = true;
+      });
+      await _loadMoreData();
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  _loadMoreData() async {
+    List<Map<String, dynamic>> loadedItems =
+        context.read<ListProvider>().loadedItems;
+
+    DocumentSnapshot? lastDocument =
+    loadedItems.isNotEmpty ? (loadedItems.last['_documentSnapshot'] as DocumentSnapshot) : null;
+
+    List<Map<String, dynamic>> moreData =
+    await FirestoreService().getDocumentStream(initialBatchSize, lastDocument).first;
+
+    // Remove duplicates by checking if the document ID already exists
+    moreData.removeWhere((newItem) =>
+        loadedItems.any((existingItem) =>
+        newItem['_documentSnapshot'].id == existingItem['_documentSnapshot'].id));
+
+    context.read<ListProvider>().addLoadedItems(moreData);
+  }
+
   @override
   Widget build(BuildContext context) {
-    var firestoreService = Provider.of<FirestoreService>(context);
-
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Row(
-              children: [
-                Text(
-                  "Animes favoritos",
-                  style: TextStyle(color: Colors.black, fontSize: 25),
-                ),
-              ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Text(
+              "Animes favoritos",
+              style: TextStyle(color: Colors.black, fontSize: 25),
             ),
-            Container(
-              height: 350.0,
-              child: FutureBuilder<List<Map<String, dynamic>>>(
-                  future: firestoreService.getDocuments(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return CircularProgressIndicator();
-                    } else if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    } else {
-                      List<Map<String, dynamic>> documents = snapshot.data!;
-
-                      return ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: documents.length,
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 8.0),
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      width: 210,
-                                      child: Text(
-                                        ('${documents[index]['Name']}'),
-                                        style: TextStyle(
-                                            color: Colors.black, fontSize: 20),
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 1,
-                                      ),
-                                    ),
-                                    GestureDetector(
-                                      onTap: () {
-                                        // Handle the onTap action here
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) => MyHomePage(
-                                                    //animeItem: AnimeItem.fromMap(documents[index]),
-                                                    )));
-                                      },
-                                      child: Container(
-                                        margin: EdgeInsets.all(10),
-                                        width: 210,
-                                        height: 300,
-                                        padding: EdgeInsets.all(20),
-                                        decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          image: DecorationImage(
-                                            image: NetworkImage(
-                                                ('${documents[index]['Image URL']}')), // Replace with your image path
-                                            fit: BoxFit
-                                                .cover, // Choose BoxFit as per your requirement
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ]),
-                            );
-                          });
-                    }
-                  }),
+            Visibility(
+              visible: isLoading,
+              child: Container(
+                margin: EdgeInsets.all(10.0),
+                child: CircularProgressIndicator(),
+              ),
             ),
-          ]);
+          ],
+        ),
+        Container(
+          height: 350.0,
+          child: Consumer<ListProvider>(
+            builder: (context, listProvider, child) {
+              return ListView.builder(
+                controller: _scrollController,
+                scrollDirection: Axis.horizontal,
+                itemCount: listProvider.loadedItems.length + (isLoading ? 1 : 0),
+                itemBuilder: (context, index) {
+                  if (index < listProvider.loadedItems.length) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 210,
+                            child: Text(
+                              ('${listProvider.loadedItems[index]['Name']}'),
+                              style:
+                              TextStyle(color: Colors.black, fontSize: 20),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              // Handle the onTap action here
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => MyHomePage(),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              margin: EdgeInsets.all(10),
+                              width: 210,
+                              height: 300,
+                              padding: EdgeInsets.all(20),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    ('${listProvider.loadedItems[index]['Image URL']}'),
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  } else {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Column(
+                        children: [
+                          // Loading indicator while more items are being loaded
+                          CircularProgressIndicator(),
+                        ],
+                      ),
+                    );
+                  }
+                },
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
--- Otaku_on_demand/lib/services/firestore.dart
+++ Otaku_on_demand/lib/services/firestore.dart
@@ -3,19 +3,43 @@ import 'package:flutter/material.dart';
 import 'package:otaku_on_demand/model/animemodel.dart';
 
 class FirestoreService {
+  DocumentSnapshot? lastDocument;
   // pegar banco de dados
   final CollectionReference animes =
       FirebaseFirestore.instance.collection('animeItem');
 
-  Future<List<Map<String, dynamic>>> getDocuments() async {
-    QuerySnapshot querySnapshot = await animes.get();
-    return querySnapshot.docs
-        .map((doc) => doc.data() as Map<String, dynamic>)
-        .toList();
+  Stream<List<Map<String, dynamic>>> getDocumentStream(int batchSize, DocumentSnapshot? lastDocument) {
+    Query query = animes
+        .orderBy('anime_id')  // Add an ordering field
+        .limit(batchSize);
+
+    if (lastDocument != null) {
+      query = query.startAfterDocument(lastDocument);
+    }
+
+    return query
+        .snapshots()
+        .map((querySnapshot) {
+      final List<Map<String, dynamic>> result = [];
+      for (final doc in querySnapshot.docs) {
+        result.add({
+          '_documentSnapshot': doc,
+          ...doc.data() as Map<String, dynamic>,
+        });
+      }
+
+      if (result.isNotEmpty) {
+        lastDocument = querySnapshot.docs.last;
+      }
+
+      return result;
+    });
   }
 
+
+
   // CREATE: add anime novo
-  Future<void> Anime(String anime) async {
+  Future<void> AnimeAdd(String anime) async {
     final docAnime = FirebaseFirestore.instance.collection('animes').doc();
 
     final anime = AnimeItem(
@@ -29,6 +53,18 @@ class FirestoreService {
   // READ: ler lista de animes
 
   // UPDATE: update um anime pelo ID
-
+  Future<void> AnimeUpdate(String anime) async {
+    final animeUpdate = FirebaseFirestore.instance.collection('animeItem').doc(
+        'anime_id');
+    animeUpdate.update({
+      'Name': 'nome novo',
+    });
+  }
   // DELETE: deletar um anime pelo ID
+  Future<void> AnimeDelete(String anime) async {
+    final animeDelete = FirebaseFirestore.instance.collection('animeItem').doc(
+        'anime_id');
+    animeDelete.delete();
+  }
+
 }
